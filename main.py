870802a4cc865305e473527bd4b1dbcc7ff3d3c9
Paul M Furley <paul@paulfurley.com>
2013-12-24 13:19:37 +0000

    Remove Ultamation specific code.
    In the quest to make a more general tool/library.

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from __future__ import unicode_literals
 
 import codecs
 import logging
-import socket
 import sys
-import threading
 import textwrap
 import os
 
@@ -15,16 +13,10 @@ import tweepy
 
 HASHTAGS = os.environ['HASHTAGS'].split(',')
 
-IP = os.environ['ULTAMATION_IP']
-UDP_PORT = int(os.environ['ULTAMATION_UDP_PORT'])
-
 _CONSUMER_KEY = os.environ['CONSUMER_KEY']
 _CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 _ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 _ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
-
-
-TIMERS = {}
 
 
 def main():
@@ -48,33 +40,6 @@ def tweet_callback(status):
             print('    - @{}\n'.format(status.author.screen_name))
             #print("'{}' in @{}: {}".format(
             #    hashtag, status.author.screen_name, status.text))
-            turn_on_five_seconds(i)
-
-
-def turn_on_five_seconds(i):
-    existing_timer = TIMERS.get(i)
-    if existing_timer:
-        existing_timer.cancel()
-
-    turn_on_now(i)
-    TIMERS[i] = threading.Timer(2, turn_off_now, [i])
-    TIMERS[i].start()
-
-
-def turn_on_now(i):
-    logging.debug("ON: #{}".format(i))
-    send_udp_data(IP, UDP_PORT, str('N:0000:1{}01:0001:1'.format(i + 2)))
-
-
-def turn_off_now(i):
-    logging.debug("OFF: #{}".format(i))
-    send_udp_data(IP, UDP_PORT, str('N:0000:1{}01:0001:0'.format(i + 2)))
-
-
-def send_udp_data(ip, port, data):
-    sock = socket.socket(socket.AF_INET,     # Internet
-                         socket.SOCK_DGRAM)  # UDP
-    sock.sendto(data, (ip, port))
 
 
 class CustomStreamListener(tweepy.StreamListener):
