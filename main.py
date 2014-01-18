ea84513dfcbef2b5a71dae9c89559be48c15b8d6
Paul M Furley <paul@paulfurley.com>
2014-01-18 11:42:17 +0000

    Fleshed out a basic echo bot to reply to @messages

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 from __future__ import unicode_literals
 
 import codecs
+import datetime
 import logging
 import sys
 import textwrap
@@ -16,28 +17,66 @@ _CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 _ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 _ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 
+_MY_NAME = os.environ['MY_NAME']
+
+_API = None
+
 
 def main():
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     auth = tweepy.auth.OAuthHandler(_CONSUMER_KEY, _CONSUMER_SECRET)
     auth.set_access_token(_ACCESS_TOKEN, _ACCESS_TOKEN_SECRET)
 
+    global _API
+    _API = tweepy.API(auth)
+
     stream = tweepy.Stream(auth, CustomStreamListener(tweet_callback))
-    stream.filter(track=KEYWORDS)  # blocking function
+    stream.filter(track=[_MY_NAME])  # blocking function
 
 
 def tweet_callback(status):
+    global _API
+
+    pretty_print_tweet(status)
+
+    if not addressed_to_me(status.text):
+        print("Ignoring '{}'".format(status.text))
+        return
+
+    message = strip_screen_names(status.text)
+
+    response = make_response_message(message)
+
+    send_tweet(status.author.screen_name, response)
+
+
+def strip_screen_names(message):
+    return message  # TODO
+
+
+def make_response_message(message):
+    return datetime.datetime.now().isoformat()  # TODO
+
+
+def send_tweet(screen_name, message):
+    global _API
+    _API.update_status('@{screen_name} {message}'.format(
+        screen_name=screen_name,
+        message=message))
+
+
+def pretty_print_tweet(status):
     #print('%s:  "%s" (%s via %s)\n' % (status.author.screen_name,
     #                                   status.text,
     #                                   status.created_at,
     #                                   status.source))
-    for i, hashtag in enumerate(KEYWORDS):
-        if hashtag.lower() in status.text.lower():
-            for line in textwrap.wrap('{}'.format(status.text)):
-                print(line)
-            print('    - @{}\n'.format(status.author.screen_name))
-            #print("'{}' in @{}: {}".format(
-            #    hashtag, status.author.screen_name, status.text))
+    for line in textwrap.wrap('{}'.format(status.text)):
+        print(line)
+    print('    - @{}\n'.format(status.author.screen_name))
+
+
+def addressed_to_me(text):
+    return text.lower().startswith(_MY_NAME.lower())
 
 
 class CustomStreamListener(tweepy.StreamListener):
