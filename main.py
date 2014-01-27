ce5f604e552efb46131428d442275e54d2ec962d
Paul M Furley <paul@paulfurley.com>
2014-01-27 23:15:25 +0000

    Replies are now generated in a separate thread
    And logging goes to file. This means the show can go on even if there's
    an error handling a response.

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from __future__ import unicode_literals
 import codecs
 import datetime
 import logging
-import sys
-import textwrap
 import os
 import re
+import sys
+import textwrap
+import threading
 
 import tweepy
 
@@ -21,8 +22,32 @@ _MY_NAME = os.environ.get('MY_NAME', '@TestAccount')
 _API = None
 
 
+class ReplyHandlerThread(threading.Thread):
+    def __init__(self, tweet):
+        threading.Thread.__init__(self)
+        self.tweet = tweet
+
+    def run(self):
+        try:
+            message = strip_screen_names(self.tweet.text)
+            response = make_response_message(message)
+
+            if response is not None:
+                logging.info("Responding: '{}'".format(response))
+                send_tweet(
+                    self.tweet.author.screen_name,
+                    response,
+                    self.tweet.id)
+
+        except Exception as e:
+            logging.exception(e)
+
+
 def main():
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        filename='output.log',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S')
     _CONSUMER_KEY = os.environ['CONSUMER_KEY']
     _CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
     _ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
@@ -48,13 +73,10 @@ def tweet_callback(status):
     if not addressed_to_me(status.text):
         logging.debug("Ignoring '{}'".format(status.text))
         return
-    message = strip_screen_names(status.text)
 
-    response = make_response_message(message)
+    handler_thread = ReplyHandlerThread(status)
+    handler_thread.start()
 
-    if response is not None:
-        logging.info("Responding: '{}'".format(response))
-        send_tweet(status.author.screen_name, response, status.id)
 
 _SCREEN_NAME_RE = re.compile(r'@[A-Za-z_]+[A-Za-z0-9_]+')
 
@@ -66,8 +88,10 @@ def strip_screen_names(message):
 def make_response_message(message):
     match = re.match(r'(?P<from>.+) to (?P<to>.+)', message)
     if match:
-        journeys = list(uktrains.search_trains(match.group('from'),
-                                               match.group('to')))
+        from_, to = match.group('from'), match.group('to')
+        logging.info("Searching '{}' to '{}'".format(from_, to))
+
+        journeys = uktrains.search_trains(from_, to)
         return describe_journey(journeys[0])
 
     if 'ping' in message.lower():
@@ -108,7 +132,7 @@ def pretty_print_tweet(status):
     #                                   status.source))
     for line in textwrap.wrap('{}'.format(status.text)):
         logging.info(line)
-    logging.info('    - @{}\n'.format(status.author.screen_name))
+    logging.info('    - @{}'.format(status.author.screen_name))
 
 
 def addressed_to_me(text):
